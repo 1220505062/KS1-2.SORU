5b4fcd9b96dea872e26aca2c92881e0acd115275
1220505062 <enesyenigun73@gmail.com>
2023-03-15 17:07:52 +0300

    KS1-2.SORU
    Bir dizi içerisindeki en küçük elemanı bulan fonksiyon kodu ve T(n) bağıntısı

---
+++ main2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define elemansayi 3
+
+    minimumbulucu (int dizi[]){
+	int i,dak;
+	dak = dizi[0];
+	for(i= 0; i<elemansayi; i++){
+		
+		if(min > dizi[i])   //elemansayisi(3) kadar çalýþacak.
+	 dak = dizi[i];   //1 defa çalýþacak.
+	 
+	 }
+	printf("en kucuk eleman : %d\n",minimum);   //1 defa çalýþacak.
+			//n+(n+1)+1+n+1+1=3n+4
+			//T(n)=3n+4
+}
+	int a(){
+ 	int sayilar[elemansayi];
+ 	int b;
+ 	for(i= 0; i<elemansayi ; i++){
+ 		printf("%d . sayiyi giriniz : " ,i+ 1);
+ 		scanf(" %d ",&sayilar[i]);
+	 }
+	 
+	 Minimumbulucu (sayilar);
+ }
